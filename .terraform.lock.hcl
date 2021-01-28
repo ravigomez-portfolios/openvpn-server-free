b0fb9856cd662e0d3c3fa54b4aae40a5960366f4
Rafael Gómez <ravigomez@gmail.com>
2021-01-28 05:42:19 +0100

    Create the network

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -2,22 +2,20 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/hashicorp/aws" {
-  version     = "2.70.0"
-  constraints = "~> 2.70"
+  version     = "3.25.0"
+  constraints = "~> 3.0"
   hashes = [
-    "h1:mM6eIaG1Gcrk47TveViXBO9YjY6nDaGukbED2bdo8Mk=",
-    "zh:01a5f351146434b418f9ff8d8cc956ddc801110f1cc8b139e01be2ff8c544605",
-    "zh:1ec08abbaf09e3e0547511d48f77a1e2c89face2d55886b23f643011c76cb247",
-    "zh:606d134fef7c1357c9d155aadbee6826bc22bc0115b6291d483bc1444291c3e1",
-    "zh:67e31a71a5ecbbc96a1a6708c9cc300bbfe921c322320cdbb95b9002026387e1",
-    "zh:75aa59ae6f0834ed7142c81569182a658e4c22724a34db5d10f7545857d8db0c",
-    "zh:76880f29fca7a0a3ff1caef31d245af2fb12a40709d67262e099bc22d039a51d",
-    "zh:aaeaf97ffc1f76714e68bc0242c7407484c783d604584c04ad0b267b6812b6dc",
-    "zh:ae1f88d19cc85b2e9b6ef71994134d55ef7830fd02f1f3c58c0b3f2b90e8b337",
-    "zh:b155bdda487461e7b3d6e3a8d5ce5c887a047e4d983512e81e2c8266009f2a1f",
-    "zh:ba394a7c391a26c4a91da63ad680e83bde0bc1ecc0a0856e26e9d62a4e77c408",
-    "zh:e243c9d91feb0979638f28eb26f89ebadc179c57a2bd299b5729fb52bd1902f2",
-    "zh:f6c05e20d9a3fba76ca5f47206dde35e5b43b6821c6cbf57186164ce27ba9f15",
+    "h1:oyaXLqVhtPnHDnwk2yU9qP4dTgfPHyuo27mX/ljeCTQ=",
+    "zh:2d3c65461bc63ec39bce7b5afdbed9a3b4dd5c2c8ee94616ad1866e24cf9b8f0",
+    "zh:2fb2ea6ccac30b909b603e183433737a30c58ec1f9a6a8b5565f0f051490c07a",
+    "zh:31a5f192c8cf29fb677cd639824f9a685578a2564c6b790517db33ea56229045",
+    "zh:437a12cf9a4d7bc92c9bf14ee7e224d5d3545cbd2154ba113ae82c4bb68edc27",
+    "zh:4bbdc3155a5dea90b2d50adfa460b0759c4dd959efaf7f66b2a0385a53b469b2",
+    "zh:63a8cd523ba31358692a34a06e111d88769576ac6d0e5adad8e0b4ae0a2d8882",
+    "zh:c4301ce86e8cb2c464949bb99e729ffe7b0c55eaf34b82ba526bb5039bca36f3",
+    "zh:c97b84861c6c550b8d2feb12d089660fffbf51dc7d660dcc9d54d4a7b3c2c882",
+    "zh:d6a103570e2d5c387b068fac4b88654dfa21d44ca1bdfa4bc8ab94c88effd71a",
+    "zh:f08cf2faf960a8ca374ac860f37c31c88ed2bab460116ac74678e0591babaac5",
   ]
 }
 
--- aws_open-vpn-free.tf
+++ aws_open-vpn-free.tf
@@ -2,17 +2,147 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.70"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
   profile = "default"
-  region  = "sa-east-1"
+  region  = var.region
 }
 
-resource "aws_instance" "openvpn-example" {
+resource "aws_vpc" "openvpn-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "openvpn-free"
+  }
+}
+
+resource "aws_internet_gateway" "openvpn-ig" {
+  vpc_id = aws_vpc.openvpn-vpc.id
+  tags = {
+    Name = "openvpn-free"
+  }
+}
+
+resource "aws_route_table" "openvpn-rt" {
+  vpc_id = aws_vpc.openvpn-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.openvpn-ig.id
+  }
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.openvpn-ig.id
+  }
+  tags = {
+    Name = "openvpn-free"
+  }
+}
+
+resource "aws_subnet" "openvpn-sn" {
+  vpc_id     = aws_vpc.openvpn-vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "openvpn-free"
+  }
+}
+
+resource "aws_route_table_association" "openvpn-rta" {
+  subnet_id      = aws_subnet.openvpn-sn.id
+  route_table_id = aws_route_table.openvpn-rt.id
+}
+
+resource "aws_security_group" "openvpn-sg" {
+  name        = "openvpn-free"
+  description = "openvpn-free"
+  vpc_id      = aws_vpc.openvpn-vpc.id
+
+  ingress {
+    description = "openvpn-free"
+    from_port   = 1194
+    to_port     = 1194
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "openvpn"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "openvpn"
+    from_port   = 945
+    to_port     = 945
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "openvpn"
+    from_port   = 943
+    to_port     = 943
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "openvpn"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "openvpn-free"
+  }
+}
+
+resource "aws_network_interface" "openvpn-ni" {
+  subnet_id       = aws_subnet.openvpn-sn.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.openvpn-sg.id]
+  tags = {
+    Name = "openvpn-free"
+  }
+}
+
+resource "aws_eip" "openvpn-eip" {
+  vpc                       = true
+  network_interface         = aws_network_interface.openvpn-ni.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_security_group.openvpn-sg]
+  tags = {
+    Name = "openvpn-free"
+  }
+}
+
+resource "aws_instance" "openvpn-free" {
   ami           = "ami-04bde880fb57a5227"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
+  key_name      = "openvpn-brazil"
+  availability_zone = "sa-east-1a"
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.openvpn-ni.id
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              echo hello|
+              EOF
+  tags = {
+    Name = "openvpn-free"
+  }
 }
